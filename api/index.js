4f87c9ab3cdcdb67e35e67f542a4464720658b6c
JuanCruz <juancruzgenovese22@gmail.com>
2023-06-13 12:21:22 -0300

    .

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const { getTypes } = require('./src/constrollers/typeControllers');
 const server = require('./src/app.js');
 const { sequelize } = require('./src/db.js');
-
+const port = process.env.PORT || 3001;
 
 sequelize.sync({ 
   force: true,
   alter: false
 })
 .then(() => {
-  server.listen(3001, () => {
+  server.listen(port, "0.0.0.0", () => {
     console.log('servidor correctamente activo y sincronizado con la DB');
   });
   getTypes()
